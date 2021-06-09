8c6d9e079249783a563a7e8f507457082350b43e
Dima <dimmaa9@gmail.com>
2021-06-09 19:49:34 +0300

    dimlom v3

--- controllers/startController.js
+++ controllers/startController.js
@@ -1,9 +1,22 @@
-const xlsx = require('node-xlsx');
+const reader = require('xlsx');
+const fs = require('fs');
+const path = require('path');
 
-exports.getStartPage = async function (req,res){
+exports.getStartPage = async function (req, res) {
     res.render('index');
 };
 
-exports.postAddFile = async function (req,res){
-    console.log(req.file);
+exports.postAddFile = async function (req, res) {
+    const file = reader.readFile(path.resolve('uploads/' + req.file.originalname))
+
+    // масив записей
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+
+    // вывести все
+    console.log(temp);
+
+    fs.unlink(path.resolve('uploads/' + req.file.originalname),function(err){
+        if(err) return console.log(err);
+        console.log('file deleted successfully');
+    });
 };
